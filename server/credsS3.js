b656f6499fcdedca1fcb0e961e69f38a7d82fe30
SergioG-SG <sergio.g.usac@gmail.com>
2021-08-31 08:29:18 -0600

    Actualización masiva Server

---
+++ server/credsS3.js
@@ -0,0 +1,10 @@
+let aws_keys = {
+    s3: {
+        region: 'us-east-2',
+        accessKeyId: "AKIA45SH54HG5KSOMZAF",
+        secretAccessKey: "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",
+        //apiVersion: '2006-03-01',
+    }
+}
+
+module.exports = aws_keys
--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
-var express = require('express');
 var bodyParser = require('body-parser');
-const cors = require('cors');
+var express = require('express');
+var AWS = require('aws-sdk');
+var cors = require('cors');
+var uuid = require('uuid');
+
 var port = 9000;
 
 var app = express();
@@ -8,14 +11,11 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.listen(port);
-
-var uuid = require('uuid');
-const aws_keys = require('./creds');
-var corsOptions = { origin: true, optionsSuccessStatus: 200 };
-
 console.log('Listening on port', port);
 
-var AWS = require('aws-sdk');
+const aws_keys = require('./credsS3');
+var corsOptions = { origin: true, optionsSuccessStatus: 200 };
+
 const s3 = new AWS.S3(aws_keys.s3);
 
 //--------------------------------------------------Prueba---------------------------------------
@@ -26,85 +26,45 @@ app.get('/', function (req, res) {
 
 });
 
-
-//--------------------------------------------------ALMACENAMIENTO---------------------------------------
-
-//subir foto en s3  EN EL PROYECTO SOLO SE SUBIRAN IMAGENES (JPG,PNG) Y ARCHIVOS PDF
 app.post('/subirfoto', function (req, res) {
 
-  var id = req.body.id;
-  var foto = req.body.foto;     //base64
-  //carpeta y nombre que quieran darle a la imagen
-
-  var nombrei = "fotos/" + id + ".jpg";
-
-  //se convierte la base64 a bytes
-  let buff = new Buffer.from(foto, 'base64');
-
-
+    var id = req.body.id;
+    var foto = req.body.foto;     //base64
+    //carpeta y nombre que quieran darle a la imagen
+    var nombrei = "fotos/" + id + ".png";
   
-  const params = {
-    Bucket: "ejemplosemiarchivos",
-    Key: nombrei,
-    Body: buff,
-    ContentType: "image",
-    ACL: 'public-read'
-  };
-
-
-
+    //se convierte la base64 a bytes
+    let buff = new Buffer.from(foto, 'base64');
   
-  const putResult = s3.putObject(params).promise();
-  res.json({ mensaje: putResult })
-
-
-
-});
-
-//subir pdf en s3
-app.post('/subirfile', function (req, res) {
-
-  var nombre = req.body.nombre;
-  var pdf = req.body.pdf;  //base 64
-  //carpeta y nombre que quieran darle al pdf
-  var nombrei = "files/" + nombre +uuid.v4()+ ".pdf";
-  //se convierte la base64 a bytes
-  let buff = new Buffer.from(pdf, 'base64');
-  const params = {
-    Bucket: "ejemplosemiarchivos",
-    Key: nombrei,
-    Body: buff,
-    ACL: 'public-read'
-  };
-
-  s3.upload(params, function sync(err, data) {
-     if (err) {
-       res.status(500).send(err)
-     } else {
-      console.log(data.Location);  
-      res.status(200).send(data);
-                        
-   }}); 
-
-
-});
+    const params = {
+      Bucket: "archivos-21-p1",
+      Key: nombrei,
+      Body: buff,
+      ContentType: "image",
+      ACL: 'public-read'
+    };
+  
+    const putResult = s3.putObject(params).promise();
+    res.json({ mensaje: putResult })
+  });
 
 //NO ES NECESARIO PARA SU PROYECTO PERO PUEDEN USARLO obtener objeto en s3
 app.post('/obtenerfoto', function (req, res) {
-  var id = req.body.id;
-  //direcccion donde esta el archivo a obtener
-  var nombrei = "fotos/" + id + ".jpg";
-  var getParams = {
-    Bucket: 'ejemplosemiarchivos',
-    Key: nombrei
-  }
-  s3.getObject(getParams, function (err, data) {
-    if (err)
-      res.json({ mensaje: "error" })
-    //de bytes a base64
-    var dataBase64 = Buffer.from(data.Body).toString('base64');
-    res.json({ mensaje: dataBase64 })
+    var id = req.body.id;
+    //direcccion donde esta el archivo a obtener
+    var nombrei = "fotos/" + id + ".png";
+    
+    var getParams = {
+      Bucket: 'archivos-21-p1',
+      Key: nombrei
+    }
 
+    s3.getObject(getParams, function (err, data) {
+      if (err)
+        res.json({ mensaje: "error" })
+      //de bytes a base64
+      var dataBase64 = Buffer.from(data.Body).toString('base64');
+      res.json({ mensaje: dataBase64 })
+  
+    });
   });
-
-});
