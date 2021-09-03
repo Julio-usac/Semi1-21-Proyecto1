6c8ccd1c0bdaeeea71319b89cadc25a13efb699a
SergioG-SG <sergio.g.usac@gmail.com>
2021-09-02 18:48:22 -0600

    1er Login

--- PaginaWeb/src/app/app.module.ts
+++ PaginaWeb/src/app/app.module.ts
@@ -1,18 +1,34 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
