f656b35731d56f803f29757cb15a929ef0ab961d
SergioG-SG <sergio.g.usac@gmail.com>
2021-09-11 15:29:25 -0600

    eliminar service actualizado

--- PaginaWeb/src/app/services/eliminar.service.ts
+++ PaginaWeb/src/app/services/eliminar.service.ts
@@ -1,11 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class EliminarService {
 
-  url:string = "http://bc-21-p1-290798132.us-east-2.elb.amazonaws.com:9000/eliminararchivo";
+  url1:string = "http://bc-21-p1-290798132.us-east-2.elb.amazonaws.com:9000/archivoseliminar";
+  url2:string = "http://bc-21-p1-290798132.us-east-2.elb.amazonaws.com:9000/eliminararchivo";
+  
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getArchivos(idusuario){
+    const data = { idusuario };
+
+    return this.httpClient.post(this.url1, data).toPromise();
+  }
+
+  delete(idarchivo, pass){
+    const data = { idarchivo, pass };
+
+    return this.httpClient.post(this.url2, data).toPromise();
+  }
 }
