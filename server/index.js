9c4b45f82598191f9b747479639050ea0a91752e
Julio-Usac <3300473961201@ingenieria.usac.edu.gt>
2021-09-12 12:00:53 -0600

    se agrego funcion

--- server/index.js
+++ server/index.js
@@ -38,6 +38,20 @@ app.get('/', function (req, res) {
 
 });
 
+app.post('/getarchivos', function (req, res) {
+
+
+  var sql="select id_archivo as idarchivo, archivo.nombre from usuario, archivo Where id_usuario="+req.body.idusuario+" and id_usuario=id_usu and tipo="+req.body.tipo+";";
+  connection.query(sql, async function(error,result){
+    if(error){
+      console.log("Error al conectar");
+      res.json({mensaje:"error"});
+    }else{
+      console.log(JSON.stringify(result));
+      res.json(result);
+    }
+  });
+});
 
 app.post('/archivoseditar', function (req, res) {
 
