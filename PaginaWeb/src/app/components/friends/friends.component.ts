a3889858625c30fb1805f821c9069768f6fd7e3c
SergioG-SG <sergio.g.usac@gmail.com>
2021-09-14 16:44:31 -0600

    implementación de búsqueda de usuarios

--- PaginaWeb/src/app/components/friends/friends.component.ts
+++ PaginaWeb/src/app/components/friends/friends.component.ts
@@ -1,17 +1,63 @@
+import { AmigosService } from 'src/app/services/amigos.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-friends',
   templateUrl: './friends.component.html',
   styleUrls: ['./friends.component.scss']
 })
+
 export class FriendsComponent implements OnInit {
 
-  buscar:string = "";
+  idUsuario:string = 'S;G';
 
-  constructor() { }
+  nombreB:string = "";
+
+  respuesta;
+  usuarios;
+
+  constructor(public AmigosService:AmigosService, private router:Router) { }
 
   ngOnInit(): void {
+    this.idUsuario = localStorage.getItem('id')
+
+    /*if (this.idUsuario == '0' || this.idUsuario == 'S;G') {
+      alert('Sesión caducada! Inicia sesión nuevamente!');
+      this.cerrarSesion();
+    }*/
+
+    this.getUsuarios();
   }
 
+  cerrarSesion() {
+    localStorage.setItem('id', '0');
+    this.router.navigate(['login'])
+  }
+
+  getFoto(foto:string) {
+    return "https://archivos-21-p1.s3.us-east-2.amazonaws.com/fotos/" + foto;
+  }
+
+  getPublicos(cantidad:string) {
+    return cantidad + " archivos publicos";
+  }
+
+  async getUsuarios() {
+    this.respuesta = await this.AmigosService.getUsuarios();
+    this.usuarios = JSON.parse(JSON.stringify(this.respuesta));
+  }
+
+  async buscar(){
+    if (this.nombreB == "") {
+      this.getUsuarios();
+    } else {
+      this.respuesta = await this.AmigosService.buscar(this.nombreB);
+    this.usuarios = JSON.parse(JSON.stringify(this.respuesta));
+    }
+  }
+
+  follow(idusuario:string){
+
+  }
 }
--- PaginaWeb/src/app/services/amigos.service.ts
+++ PaginaWeb/src/app/services/amigos.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,5 +7,20 @@ import { Injectable } from '@angular/core';
 
 export class AmigosService {
 
-  constructor() { }
+  /*url1:string = "http://bc-21-p1-290798132.us-east-2.elb.amazonaws.com:9000/getusuarios";
+  url2:string = "http://bc-21-p1-290798132.us-east-2.elb.amazonaws.com:9000/getusuario";*/
+  url1:string = "http://localhost:9000/getusuarios";
+  url2:string = "http://localhost:9000/getusuario";
+
+  constructor(private httpClient: HttpClient) { }
+
+  getUsuarios() {
+    return this.httpClient.get(this.url1).toPromise();
+  }
+
+  buscar(nombre:string) {
+    const data = { nombre };
+
+    return this.httpClient.post(this.url2, data).toPromise();
+  }
 }
